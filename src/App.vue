922a5541f81fbe8541eaaa2c76b99ad190e22a57
Darsiuz <dggalvezm@gmail.com>
2025-11-28 21:10:46 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const newTask = ref<string>('')
+const tasks = ref<string[]>([])
+
+const Agregar = () => {
+  if (newTask.value.trim().length === 0) return
+  tasks.value.push(newTask.value)
+  newTask.value = ''
+}
+</script>
+
+<template>
+  <div>
+    <h1>Lista de tareas</h1>
+    <input v-model="newTask" placeholder="Nueva tarea" />
+
+    <button @click="Agregar">Agregar</button>
+
+    <ul>
+      <li v-for="(task, i) in tasks" :key="i">
+        {{ task }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+input {
+  margin-right: 8px;
+}
+</style>
