996c4b90a226e1626f834b84cd630da48909cbb1
Darsiuz <dggalvezm@gmail.com>
2025-11-28 21:37:23 -0500

    cambiar idioma a español en index.html y mejorar la interfaz de la lista de tareas en App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,28 @@ const Agregar = () => {
   tasks.value.push(newTask.value)
   newTask.value = ''
 }
+const Eliminar = (index: number) => {
+  tasks.value.splice(index, 1)
+}
 </script>
 
 <template>
   <div>
     <h1>Lista de tareas</h1>
-    <input v-model="newTask" placeholder="Nueva tarea" />
+    <div class="input-group mb-3">
+      <input v-model="newTask" placeholder="Nueva tarea" class="form-control" />
+      <button class="btn btn-primary" @click="Agregar">Agregar</button>
+    </div>
 
-    <button @click="Agregar">Agregar</button>
-
-    <ul>
-      <li v-for="(task, i) in tasks" :key="i">
-        {{ task }}
+    <ul class="list-group list-group-flush">
+      <li v-for="(task, index) in tasks" :key="index" class="list-group-item center">
+        <div class="d-flex justify-content-between align-items-center">
+          <span>{{ task }}</span>
+          <button class="btn btn-danger btn-sm float-end" @click="Eliminar(index)">X</button>
+        </div>
       </li>
     </ul>
+
   </div>
 </template>
 
